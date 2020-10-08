46df59abbe2c95f9e191d99397a08a54fcb510da
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2020-10-08 17:53:59 +0200

    Весело

---
+++ lab_ex6+.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  8 17:26:12 2020
+
+@author: student
+"""
+a, b = input().split(', ')
+a = int(a)
+if 'до' in b:
+    x = (776 - a) // 4 + 1
+    y = (776 - a) % 4 + 1
+else:
+    x = (776 + a + 1) // 4 + 1
+    y = (776 + a + 1) % 4 + 1
+print('OL', ' ', x, '.',  y, sep='')
