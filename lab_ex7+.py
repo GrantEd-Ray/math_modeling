3ac607518f01d244201f7dfdf7101f4c454ed165
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2020-10-08 18:00:17 +0200

    Обидели (слишком легко)

---
+++ lab_ex7+.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  8 17:54:10 2020
+
+@author: student
+"""
+
+for i in range(1, 10):
+    for j in range(1, 10):
+        print(i * j, end='\t')
+    print('', end='\n')
+
+
+
+
