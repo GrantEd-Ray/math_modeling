8fbbce219a995deb1e6db39cf6629a7a093df303
GrantEd-Ray <71932889+GrantEd-Ray@users.noreply.github.com>
2020-11-19 18:38:35 +0200

    Hohohohoho

---
+++ lab5ex4.py
@@ -0,0 +1,10 @@
+from sympy import symbols
+from sympy.solvers.inequalities import reduce_abs_inequality
+from sympy.solvers.inequalities import reduce_rational_inequalities
+from sympy import Abs
+
+x = symbols('x')
+
+print(reduce_abs_inequality(Abs(x**2 + 2*x - 3) + 3*(x + 1), '<', x))
+
+print(reduce_rational_inequalities([[(x - 1) * (x + 2) / ((x - 3) * (x + 4)) <= 0]], x))
